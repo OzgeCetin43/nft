8d0a76b575d119f9abd3ed3eac7bb5622e97c9ee
OzgeCetin43 <32112164+OzgeCetin43@users.noreply.github.com>
2022-04-20 14:52:04 +0300

    deploy my website

---
+++ src/assets/data/data.js
@@ -0,0 +1,103 @@
+import creator1 from "../images/creator1.jpg";
+import creator2 from "../images/creator2.jpg";
+import creator3 from "../images/creator3.jpg";
+
+export const NFT_DATA = [
+    {
+        id: 1,
+        name: "Lion 1",
+        image: "product-item-image-1",
+        creator: "Derek Grey",
+        creatorImage: creator1,
+        description: "The lion (Panthera leo) is a large cat of the genus Panthera native to Africa and India. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane. It is a social species, forming groups called prides. A lion's pride consists of a few adult males, related females, and cubs. Groups of female lions usually hunt together, preying mostly on large ungulates. The lion is an apex and keystone predator; although some lions scavenge when opportunities occur and have been known to hunt humans, the species typically does not."
+    },
+    {
+        id: 2,
+        name: "Tiger 1",
+        image: "product-item-image-2",
+        creator: "Alex Benjamin",
+        creatorImage: creator2,
+        description: "The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside. An apex predator, it primarily preys on ungulates such as deer and wild boar. It is territorial and generally a solitary but social predator, requiring large contiguous areas of habitat, which support its requirements for prey and rearing of its offspring. Tiger cubs stay with their mother for about two years, then become independent and leave their mother's home range to establish their own."
+
+    },
+    {
+        id: 3,
+        name: "Lion 2",
+        image: "product-item-image-3",
+        creator: "Derek Grey",
+        creatorImage: creator1,
+        description: "The lion (Panthera leo) is a large cat of the genus Panthera native to Africa and India. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane. It is a social species, forming groups called prides. A lion's pride consists of a few adult males, related females, and cubs. Groups of female lions usually hunt together, preying mostly on large ungulates. The lion is an apex and keystone predator; although some lions scavenge when opportunities occur and have been known to hunt humans, the species typically does not."
+    },
+    {
+        id: 4,
+        name: "Dog 1",
+        image: "product-item-image-4",
+        creator: "Tom Adel",
+        creatorImage: creator3,
+        description: "The dog or domestic dog (Canis familiaris or Canis lupus familiaris) is a domesticated descendant of the wolf, and is characterized by an upturning tail. The dog is derived from an ancient, extinct wolf, and the modern wolf is the dog's nearest living relative. The dog was the first species to be domesticated, by hunter–gatherers over 15,000 years ago, before the development of agriculture. Due to their long association with humans, dogs have expanded to a large number of domestic individuals and gained the ability to thrive on a starch-rich diet that would be inadequate for other canids."
+    },
+    {
+        id: 5,
+        name: "Monkey 1",
+        image: "product-item-image-5",
+        creator: "Alex Benjamin",
+        creatorImage: creator2,
+        description: "Monkey is a common name that may refer to most mammals of the infraorder Simiiformes, also known as the simians. Traditionally, all animals in the group now known as simians are counted as monkeys except the apes, a grouping known as paraphyletic; however in the broader sense based on cladistics, apes (Hominoidea) are also included, making the terms monkeys and simians synonyms in regard of their scope. Monkeys are divided into the families of New World monkeys (Platyrrhini) and Old World monkeys (Cercopithecidae in the strict sense; Catarrhini in the broad sense, which again includes apes)."
+    },
+    {
+        id: 6,
+        name: "Tiger 2",
+        image: "product-item-image-6",
+        creator: "Tom Adel",
+        creatorImage: creator3,
+        description: "The tiger (Panthera tigris) is the largest living cat species and a member of the genus Panthera. It is most recognisable for its dark vertical stripes on orange fur with a white underside. An apex predator, it primarily preys on ungulates such as deer and wild boar. It is territorial and generally a solitary but social predator, requiring large contiguous areas of habitat, which support its requirements for prey and rearing of its offspring. Tiger cubs stay with their mother for about two years, then become independent and leave their mother's home range to establish their own."
+    },
+    {
+        id: 7,
+        name: "Dog 2",
+        image: "product-item-image-7",
+        creator: "Derek Grey",
+        creatorImage: creator1,
+        description: "The dog or domestic dog (Canis familiaris or Canis lupus familiaris) is a domesticated descendant of the wolf, and is characterized by an upturning tail. The dog is derived from an ancient, extinct wolf, and the modern wolf is the dog's nearest living relative. The dog was the first species to be domesticated, by hunter–gatherers over 15,000 years ago, before the development of agriculture. Due to their long association with humans, dogs have expanded to a large number of domestic individuals and gained the ability to thrive on a starch-rich diet that would be inadequate for other canids."
+    },
+    {
+        id: 8,
+        name: "Monkey 2",
+        image: "product-item-image-8",
+        creator: "Tom Adel",
+        creatorImage: creator3,
+        description: "Monkey is a common name that may refer to most mammals of the infraorder Simiiformes, also known as the simians. Traditionally, all animals in the group now known as simians are counted as monkeys except the apes, a grouping known as paraphyletic; however in the broader sense based on cladistics, apes (Hominoidea) are also included, making the terms monkeys and simians synonyms in regard of their scope. Monkeys are divided into the families of New World monkeys (Platyrrhini) and Old World monkeys (Cercopithecidae in the strict sense; Catarrhini in the broad sense, which again includes apes)."
+    },
+    {
+        id: 9,
+        name: "Lion 3",
+        image: "product-item-image-9",
+        creator: "Derek Grey",
+        creatorImage: creator1,
+        description: "The lion (Panthera leo) is a large cat of the genus Panthera native to Africa and India. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane. It is a social species, forming groups called prides. A lion's pride consists of a few adult males, related females, and cubs. Groups of female lions usually hunt together, preying mostly on large ungulates. The lion is an apex and keystone predator; although some lions scavenge when opportunities occur and have been known to hunt humans, the species typically does not."
+    },
+    {
+        id: 10,
+        name: "Monkey 3",
+        image: "product-item-image-10",
+        creator: "Alex Benjamin",
+        creatorImage: creator2,
+        description: "Monkey is a common name that may refer to most mammals of the infraorder Simiiformes, also known as the simians. Traditionally, all animals in the group now known as simians are counted as monkeys except the apes, a grouping known as paraphyletic; however in the broader sense based on cladistics, apes (Hominoidea) are also included, making the terms monkeys and simians synonyms in regard of their scope. Monkeys are divided into the families of New World monkeys (Platyrrhini) and Old World monkeys (Cercopithecidae in the strict sense; Catarrhini in the broad sense, which again includes apes)."
+    },
+    {
+        id: 11,
+        name: "Dog 3",
+        image: "product-item-image-11",
+        creator: "Tom Adel",
+        creatorImage: creator3,
+        description: "The dog or domestic dog (Canis familiaris or Canis lupus familiaris) is a domesticated descendant of the wolf, and is characterized by an upturning tail. The dog is derived from an ancient, extinct wolf, and the modern wolf is the dog's nearest living relative. The dog was the first species to be domesticated, by hunter–gatherers over 15,000 years ago, before the development of agriculture. Due to their long association with humans, dogs have expanded to a large number of domestic individuals and gained the ability to thrive on a starch-rich diet that would be inadequate for other canids."
+    },
+    {
+        id: 12,
+        name: "Lion 4",
+        image: "product-item-image-12",
+        creator: "Derek Grey",
+        creatorImage: creator1,
+        description: "The lion (Panthera leo) is a large cat of the genus Panthera native to Africa and India. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions are larger than females and have a prominent mane. It is a social species, forming groups called prides. A lion's pride consists of a few adult males, related females, and cubs. Groups of female lions usually hunt together, preying mostly on large ungulates. The lion is an apex and keystone predator; although some lions scavenge when opportunities occur and have been known to hunt humans, the species typically does not."
+    },
+]
--- src/pages/Market/index.jsx
+++ src/pages/Market/index.jsx
@@ -1,9 +1,99 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Market.css";
+import hero from "../../assets/images/market-hero.png";
+import eth from "../../assets/images/eth.png";
+import { Link } from "react-router-dom";
+import {NFT_DATA} from "../../assets/data/data";
 
 const Market = (props) => {
+  const [filterAreaShowable, setFilterAreaShowable] = useState(false);
+
   return (
-    <div>Market</div>
+    <>
+      <div className="market-hero-container">
+        <div className="left">
+          <small className="subtitle">Largest NFT marketplace on the world!</small>
+          <h1 className="title">NFT Marketplace</h1>
+          <p className="description">Explore on the world's best & largest NFT marketplace with our beautiful NFT products. We want to be a part of your smile, success and future growth.</p>
+          <p className="description">Do you want to create your own NFTs?</p>
+          <Link to="/nft/createNft" className="create-btn">Create</Link>
+          <div className="about">
+            <div className="about-inner">
+              <p className="about-title">28K+</p>
+              <p className="about-description">Artwork</p>
+            </div>
+            <div className="about-inner">
+              <p className="about-title">18K+</p>
+              <p className="about-description">Auction</p>
+            </div>
+            <div className="about-inner">
+              <p className="about-title">8K+</p>
+              <p className="about-description">Artist</p>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <img src={hero} alt="nft, marketplace, ethereum" className="hero" />
+        </div>
+      </div>
+      <div className="marketplace">
+        <div className="filter-area">
+          <h1 className="title">Explore Products</h1>
+          <button className="filter-btn" onClick={() => setFilterAreaShowable(!filterAreaShowable)}>Filter {filterAreaShowable ? <i class="ri-close-fill"></i> : <i class="ri-filter-fill"></i>}</button>
+        </div>
+        {
+          filterAreaShowable && <div className="filters">
+            <select className="filter">
+              <option>All Categories</option>
+              <option value="dog">Dog</option>
+              <option value="lion">Lion</option>
+              <option value="monkey">Monkey</option>
+              <option value="tiger">Tiger</option>
+            </select>
+            <select className="filter">
+              <option>All Items</option>
+              <option value="single-item">Single Item</option>
+              <option value="bundle">Bundle</option>
+            </select>
+            <select className="filter">
+              <option>Sort by</option>
+              <option value="high-rate">High Rate</option>
+              <option value="mid-rate">Mid Rate</option>
+              <option value="low-rate">Low Rate</option>
+            </select>
+          </div>
+        }
+        <div className="products">
+          {
+            NFT_DATA.map(data => {
+              return (
+                <div key={data.id} className="product-item">
+                  <div className={data.image}/>
+                  <h2 className="title">{data.name}</h2>
+                  <div className="info">
+                    <div className="creator">
+                      <img src={data.creatorImage} alt="nft, ethereum, marketplace" className="creator-image"/>
+                      <div className="creator-inner">
+                        <small className="subtitle">Owned by</small>
+                        <p className="name">{data.creator}</p>
+                      </div>
+                    </div>
+                    <div className="bid">
+                      <div className="creator-inner">
+                        <small className="subtitle">Current Bid</small>
+                        <p className="price">10 ETH</p>
+                      </div>
+                    </div>
+                    <img src={eth} alt="nft, marketplace, ethereum" className="eth"/>
+                  </div>
+                  <button className="bid-btn">Place a Bid</button>
+                </div>
+              )
+            })
+          }
+        </div>
+       </div>
+    </>
   )
 }
 
