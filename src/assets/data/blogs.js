c57caf10ebdce6a4a9d9bf5a1b588a69620e1ef7
OzgeCetin43 <32112164+OzgeCetin43@users.noreply.github.com>
2022-06-22 17:41:55 +0300

    some code improvements

--- src/assets/data/blogs.js
+++ src/assets/data/blogs.js
@@ -17,7 +17,7 @@ export const BLOG_DATA = [
         description: "What are NFTs? Where they get the value from? Are they here to stay? We will answer all of it in easy way.",
         date: "APRIL 10, 2021",
         descriptionLong: "NFT = Non Fungible Token, just kidding. Think of an NFT as birth certificate, unique and one of a kind, it's proof that it's you. It cannot be replaced with anything else. To compare NFTs to what is already out there, Bitcoin and Ethereum are fungible tokens you can exchange them between each other, you cannot exchange your birth certificate with another person so that is pretty much what NFT is.",
-        image: blog2
+        image: blog3
     },
     {
         id: 3,
@@ -25,6 +25,6 @@ export const BLOG_DATA = [
         description: "Coined from a cyberpunk novel in the ‘90s, the metaverse has been the crux of tech industry discourse.",
         date: "FEBRUARY 3, 2022",
         descriptionLong: "This year, the metaverse has been the poster word of most tech and crypto websites. Tech analysts have been theorizing the next iteration of the internet, an idea where users get to interact and live many lives (with avatars) in a non-physical world. This abstract term, often traced to a dystopian novel in the early ’90s, has become the highlight of the tech industry discourse, thanks to big institutional moves, including the Facebook rebrand and Roblox’s initial public offering. Of course, every frontier in tech offers opportunities to propel humanity forward and ultimately make lives easier. But, on the flip side, solutions must be critically examined, and from the investor’s perspective, the prospect is what matters, and this piece briefly looks at the potential outlook for the metaverse in 2022.",
-        image: blog3
+        image: blog2
     },
 ]
