5f2bc8e2dc7574a080ce1604536b47f5750bb11c
OzgeCetin43 <32112164+OzgeCetin43@users.noreply.github.com>
2022-06-22 17:58:41 +0300

    blog page was added.

--- src/assets/data/blogs.js
+++ src/assets/data/blogs.js
@@ -9,7 +9,7 @@ export const BLOG_DATA = [
         description: "Want to learn more about how to create your own NFT community to succeed in the NFTs space? We tell you everything in this guide.",
         date: "MAY 9, 2022",
         descriptionLong: "Are you new to the world of non-fungible tokens (NFTs) or perhaps an artist, collector or enthusiast? Then you should join an NFT community, or at least create one, where most NFT creators will learn from each other, engage and share ideas. With social media channels such as Twitter, Telegram, or Discord, ... etc. NFT enthusiasts invest time, collaborate, support and purchase each other's artwork and other NFT projects. Popular NFT projects such as Cryptopunks, Axie Infinity, Bored Ape Yacht Club, ... are getting international attention because of the strong NFT communities they have built around them for some time now. How can you create your own NFT community to succeed in the NFT space as well? This guide will give you an overview of best practices to implement.",
-        image: blog1
+        image: blog3
     },
     {
         id: 2,
@@ -17,7 +17,7 @@ export const BLOG_DATA = [
         description: "What are NFTs? Where they get the value from? Are they here to stay? We will answer all of it in easy way.",
         date: "APRIL 10, 2021",
         descriptionLong: "NFT = Non Fungible Token, just kidding. Think of an NFT as birth certificate, unique and one of a kind, it's proof that it's you. It cannot be replaced with anything else. To compare NFTs to what is already out there, Bitcoin and Ethereum are fungible tokens you can exchange them between each other, you cannot exchange your birth certificate with another person so that is pretty much what NFT is.",
-        image: blog3
+        image: blog1
     },
     {
         id: 3,
